2e91d4f4698dfcf89cc870cfd447660098857dc7
Mariano <perezm7401@gmail.com>
2022-06-01 13:25:57 -0300

    Creo proyecto de testing

---
+++ TP02_Exceptions_ExtMethods_Testing/TP02_Exceptions_ExtMethods_TestingTests/src/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP02_Exceptions_ExtMethods_Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP02_Exceptions_ExtMethods_Testing.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SquareRootTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ParseUserInputTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
