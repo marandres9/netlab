3f8e3e19bf27dcae8d5838736264160b091e85c6
Mariano <perezm7401@gmail.com>
2022-06-01 13:10:25 -0300

    Creo nuevo proyecto

---
+++ TP02_Exceptions_ExtMethods_Testing/TP02_Exceptions_ExtMethods_Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP02_Exceptions_ExtMethods_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
