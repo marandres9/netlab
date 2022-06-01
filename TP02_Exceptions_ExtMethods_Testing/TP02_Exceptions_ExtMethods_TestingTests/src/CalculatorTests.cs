5534c2153535aa83bc03d90fecc9dc83f8a8d75e
Mariano <perezm7401@gmail.com>
2022-06-01 13:29:19 -0300

    Implemento unit-tests para metodos de la clase calculadora

--- TP02_Exceptions_ExtMethods_Testing/TP02_Exceptions_ExtMethods_TestingTests/src/CalculatorTests.cs
+++ TP02_Exceptions_ExtMethods_Testing/TP02_Exceptions_ExtMethods_TestingTests/src/CalculatorTests.cs
@@ -14,19 +14,58 @@ namespace TP02_Exceptions_ExtMethods_Testing.Tests
         [TestMethod()]
         public void DivideTest()
         {
-            Assert.Fail();
+            var calc = new Calculator();
+            double expected = 1.5;
+            double dividend = 3.0;
+            double divisor = 2.0;
+
+            Assert.AreEqual(expected, calc.Divide(dividend, divisor));
         }
 
         [TestMethod()]
         public void SquareRootTest()
         {
-            Assert.Fail();
+            var calc = new Calculator();
+
+            double expected = 9d;
+            double radicand = 81d;
+
+            Assert.AreEqual(expected, calc.SquareRoot(radicand));
         }
 
         [TestMethod()]
         public void ParseUserInputTest()
         {
-            Assert.Fail();
+            var calc = new Calculator();
+            double expected = 4.86;
+            string input = "4.86";
+
+            Assert.AreEqual(expected, calc.ParseUserInput(input));
         }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void DivideByZeroTest()
+        {
+            var calc = new Calculator();
+            calc.Divide(10, 0);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exceptions.NegataiveRadicandException))]
+        public void NegativeRadicandTest()
+        {
+            var calc = new Calculator();
+            calc.SquareRoot(-4);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exceptions.InvalidInputException))]
+        public void InvalidInputTest()
+        {
+            var calc = new Calculator();
+            calc.ParseUserInput("a");
+        }
+
     }
 }
