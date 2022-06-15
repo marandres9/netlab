e8a051d73a46be56526b03f7ef06d5394e503712
Mariano <perezm7401@gmail.com>
2022-06-15 15:02:49 -0300

    agrego comentarios y correciones finales

--- TP06HTML/js/script.js
+++ TP06HTML/js/script.js
@@ -1,22 +1,23 @@
-const form = document.getElementById('form');
-
-const heroText = document.querySelector('.hero-text');
 var entries = 0;
 
-const btnScrollToForm = document.getElementById('btn-scrollToForm');
+const form = document.getElementById('form');
 
+// ejemplo de event binding usando jQuery
 $('#btn-scrollToForm').click(() => {
     form.scrollIntoView(true);
 });
 
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
+    // obtiene los elementos del formulario
     const fname = form.elements['first-name'];
     const lname = form.elements['last-name'];
     const birthDate = form.elements['birth-date'];
     const age = { value: getAge(birthDate.value).toString() };
     const adress = form.elements['adress'];
 
+    // valida los datos ingresados
     if (!fname.value || !lname.value) {
         document.getElementById('invalid-names').style.display = 'block';
         return;
@@ -32,21 +33,27 @@ form.addEventListener('submit', function (event) {
         document.getElementById('invalid-adress').style.display = 'block';
         return;
     }
+
+    // busca la tabla en el DOM, crea una nueva fila y le agrega los datos validados
     const table = document.getElementById('table');
     const row = document.createElement('tr');
     const formEntry = [fname, lname, age, birthDate, adress];
-    for (item of formEntry) {
+    for (let item of formEntry) {
         let data = document.createElement('td');
         data.innerText = item.value;
         row.appendChild(data);
     }
+    // agrega la fila a la tabla
     table.append(row);
+
     updateHeroText();
-    // form.reset();
+    form.reset();
     window.scroll(0, 0)
 });
 
 form.addEventListener('reset', function (event) {
+    // al provocar un reset en el formulario elimina todas las alertas que se muestran si
+    // el usuario ingresa datos invalidos
     const alerts = Array.from(document.querySelectorAll('.alert-danger'));
     for (let alert of alerts) {
         alert.style.display = 'none';
@@ -54,6 +61,7 @@ form.addEventListener('reset', function (event) {
 });
 
 function getAge(birthDateString) {
+    // calcula la edad de la persona
     let today = new Date();
     let birthDate = new Date(birthDateString + 'T00:00:00');
     let years = today.getFullYear() - birthDate.getFullYear();
@@ -66,6 +74,7 @@ function getAge(birthDateString) {
 }
 
 function updateHeroText() {
-    entries++;
-    heroText.innerHTML = `You\'ve added ${entries} entries`;
+    // muestra al usuario cuantos veces agrego datos a la tabla
+    const heroText = document.getElementById('hero-text');
+    heroText.innerHTML = `You\'ve added ${++entries} entries`;
 }
