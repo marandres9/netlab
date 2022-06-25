fd03141cb6c717927f28fbcd95fce320ca402672
Mariano <perezm7401@gmail.com>
2022-06-25 18:29:38 -0300

    created home & not-found components

--- ng-TP09Angular/src/app/app.module.ts
+++ ng-TP09Angular/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HomeModule } from './features/home/home.module'
+import { SharedModule } from './shared/shared.module'
+import { NotFoundModule } from './features/not-found/not-found.module'
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HomeModule,
+        NotFoundModule,
+        SharedModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
