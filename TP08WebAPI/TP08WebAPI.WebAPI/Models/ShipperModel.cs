637fbd151768f34496f9d3f317fce1e3fe3eb200
Mariano <perezm7401@gmail.com>
2022-06-27 01:34:07 -0300

    Created Shippers model, returned by api. Includes only info necessary to display a list

---
+++ TP08WebAPI/TP08WebAPI.WebAPI/Models/ShipperModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP08WebAPI.WebAPI.Models
+{
+    public class ShipperModel
+    {
+        public int ShipperID { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
