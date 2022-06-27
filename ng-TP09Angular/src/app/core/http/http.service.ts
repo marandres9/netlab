5881c924c819be4da8a1e36be3c25667cdd995d9
Mariano <perezm7401@gmail.com>
2022-06-27 19:47:17 -0300

    added url environment variable

--- ng-TP09Angular/src/app/core/http/http.service.ts
+++ ng-TP09Angular/src/app/core/http/http.service.ts
@@ -1,24 +1,23 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { environment } from 'src/environments/environment'
 import { Shipper, ShipperDetails } from '../../shared/models/Shipper'
 
 @Injectable({
     providedIn: 'root',
 })
 export class HttpService {
-    private url = 'https://localhost:44359'
-
     constructor(private http: HttpClient) {}
 
     getAllShippers() {
-        return this.http.get<Shipper[]>(`${this.url}/api/shippers/`)
+        return this.http.get<Shipper[]>(environment.url + 'api/shippers/')
     }
 
     getDetailsShippers(id: number) {
-        return this.http.get<ShipperDetails>(`${this.url}/api/shippers/${id}`)
+        return this.http.get<ShipperDetails>(environment.url + `api/shippers/${id}`)
     }
 
     postShipper(shipper: Shipper) {
-        return this.http.post<Shipper>(`${this.url}/api/shippers/add`, shipper)
+        return this.http.post<Shipper>(environment.url + 'api/shippers/add', shipper)
     }
 }
