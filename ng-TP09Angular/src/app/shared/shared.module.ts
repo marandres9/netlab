68be00cda0191f991e33d8186cefd1f28403e349
Mariano <perezm7401@gmail.com>
2022-06-30 02:28:41 -0300

    Moved material modules to shared-module. Updated toolbar

--- ng-TP09Angular/src/app/shared/shared.module.ts
+++ ng-TP09Angular/src/app/shared/shared.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
+
 import { ToolbarComponent } from './components/toolbar/toolbar.component'
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { StopClickPropagationDirective } from './directives/stop-click-propagation.directive'
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatListModule } from '@angular/material/list'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatTreeModule } from '@angular/material/tree'
 
 @NgModule({
     declarations: [ToolbarComponent, StopClickPropagationDirective],
-    imports: [CommonModule, MatToolbarModule],
-    exports: [ToolbarComponent, StopClickPropagationDirective],
+    imports: [CommonModule, MatToolbarModule, MatButtonModule, RouterModule],
+    exports: [
+        ToolbarComponent,
+        StopClickPropagationDirective,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatExpansionModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        RouterModule,
+        MatTreeModule
+    ],
 })
 export class SharedModule {}
